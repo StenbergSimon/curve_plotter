b5ce1dcbc7e8cd8a718131117cc7e372e5956ca7
StenbergSimon <simon.stenberg@nmbu.no>
2015-11-18 23:00:07 +0100

    moved import of arguments into wrapper

--- extract_curves.py
+++ extract_curves.py
@@ -3,14 +3,7 @@ import pandas as pd
 import numpy as np
 from optparse import OptionParser as opt
 import os
-
-# This part handles the flags for input and such
-prsr = opt()
-prsr.add_option("-m", "--meta", dest="meta", metavar="FILE", help="Meta data in EXCEL format. Should be structured as used in scan-o-matic with 4 tabs")
-prsr.add_option("-i", "--input", dest="curves", metavar="FILE", help="Input file with curve data. Select either curves_raw.npy for raw curves, or curves_smooth.npy for smoothened curves.")
-prsr.add_option("-p", "--plate", dest="no", metavar="INT", help="Plate (0-3) to process")
-prsr.add_option("-o", "--output", dest="path", metavar="PATH", default=os.getcwd(), help="Output path Default:%default")
-(options, args) = prsr.parse_args()
+from plotter_wrapper.py import options
 
 # Here some functions are defined
 
