992d2506c138214a328518c683b64838cf7f373d
StenbergSimon <simon.stenberg@nmbu.no>
2015-11-18 23:59:04 +0100

    Moved code

--- extract_curves.py
+++ extract_curves.py
@@ -1,9 +1,9 @@
 # Importing some required libraries 
-import pandas as pd
-import numpy as np
-from optparse import OptionParser as opt
 import os
-from plotter_wrapper.py import options
+import pandas as pd
+
+# List each ORF
+files = []
 
 # Here some functions are defined
 
@@ -21,6 +21,7 @@ def annotate_pos(table, plate_no):
 # This fuction extracts the curves
 def extract_curves(curves, meta_data, plate_no, outpath):
         for ORF in meta_data[0].unique():
+		files.append(ORF)
 		temp_table = meta_data.loc[meta_data[0] == ORF]
 		df = pd.DataFrame()
 		i = 1
@@ -30,13 +31,4 @@ def extract_curves(curves, meta_data, plate_no, outpath):
 			i = i + 1
 		df.to_csv(os.path.join(os.path.abspath(outpath),str(ORF)))
 		
-
-
-# Here the script reads the files and store them in memory
-meta_data = pd.read_excel(options.meta, sheetname=None, header=None)
-curves = np.load(options.curves)
-
-# Here the program actually runs
-plate = annotate_pos(meta_data, options.no)
-extract_curves(curves, plate, options.no, options.path)
 
