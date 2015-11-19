29cc10f2c1ee6fadf9cb08ef8b4a5bbc306c5816
StenbergSimon <simon.stenberg@nmbu.no>
2015-11-19 03:12:08 +0100

    Fixed correct flags

--- plotter_wrapper.py
+++ plotter_wrapper.py
@@ -10,8 +10,11 @@ prsr.add_option("-m", "--meta", dest="meta", metavar="FILE", help="Meta data in 
 prsr.add_option("-i", "--input", dest="curves", metavar="FILE", help="Input file with curve data. Select either curves_raw.npy for raw curves, or curves_smooth.npy for smoothened curves.")
 prsr.add_option("-p", "--plate", dest="no", metavar="INT", help="Plate (0-3) to process")
 prsr.add_option("-o", "--output", dest="path", metavar="PATH", default=os.getcwd(), help="Output path Default:%default")
-prsr.add_option("-n", "--norm_amp", dest="norm", metavar="BOOLEAN", default="False", help="Set True to push amplitude of all curves to the highest one. Default:%default")
-prsr.add_option("-z", "--overlay", dest="over", metavar="BOOLEAN", default="False", help="Plot overlay? Default:%default")
+prsr.add_option("-n", dest="norm", action="store_true", help="Set True to push amplitude of all curves to the highest one.")
+prsr.add_option("-z", dest="over", action="store_true", help="plot overlay")
+
+#prsr.add_option("-n", "--norm_amp", dest="norm", metavar="BOOLEAN", default="False", help="Set True to push amplitude of all curves to the highest one. Default:%default")
+#prsr.add_option("-z", "--overlay", dest="over", metavar="BOOLEAN", default="False", help="Plot overlay? Default:%default")
 (options, args) = prsr.parse_args()
 
 if __name__ == "__main__":
@@ -26,13 +29,13 @@ if __name__ == "__main__":
 	fh = open(os.path.join(options.path,"list.txt"),"w")
 	for data in extract_curves.files:	
 		fh.write("%s\n" % data)
-		if bool(options.over):
+		if not options.over:
 			call(["Rscript", str(plotter), str(data)],stdout=DEVNULL, stderr=DEVNULL)
 			call(["rm", str(data)])
 		
 	fh.close()
-	if not bool(options.over):
-		call(["Rscript", str(plotter), os.path.join(options.path,"list.txt")])
+	if options.over:
+		call(["Rscript", str(plotter_over), os.path.join(options.path,"list.txt")],stdout=DEVNULL, stderr=DEVNULL)
 		for data in extract_curves.files:
 			call(["rm", str(data)])
 	
