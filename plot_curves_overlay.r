585cdf3027e909f54675a1d5505c116918a31774
StenbergSimon <simon.stenberg@nmbu.no>
2015-11-23 19:36:39 +0100

    Added standard deviation area around curves

--- plot_curves_overlay.r
+++ plot_curves_overlay.r
@@ -1,4 +1,4 @@
-for (package in c('ggplot2', 'Rmisc', 'reshape2')) {
+for (package in c('ggplot2','grid', 'Rmisc', 'reshape2')) {
     if (!require(package, character.only=T, quietly=T)) {
         install.packages(package, dependencies=TRUE)
         library(package, character.only=T)
@@ -17,8 +17,9 @@ x.sum <- summarySE(data=x.molten, groupvars=c("X", "name"), na.rm=TRUE, measurev
 z <- rbind(z,x.sum)}
 
 #z.sum <- summarySE(data=z, groupvars=c("X","variable") , na.rm=TRUE, measurevar="value")
-p <- ggplot(z, aes(x=X, y=value, color=name))
+p <- ggplot(z, aes(x=X, y=value, fill=name))
 p <- p + geom_line() + scale_y_continuous(trans="log2") + annotation_logticks(base=2, sides="l") + labs(x="Time", y="Cells (log2)") + theme_classic(base_size=15) + theme(legend.position="none")
+p <-  p + geom_ribbon(aes(ymin=value-sd, ymax=value+sd), alpha=0.5)
 pdf(paste("summary",".pdf",sep=""))
 print(p)
 dev.off()
